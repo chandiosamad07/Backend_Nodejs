ea17a190e65b1eb27f7bf6c0b568c44c14c3825d
chandiosamad07 <absamadchandio@gmamil.com>
2024-06-28 11:11:47 +0500

    Simple server using nodejs

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const fs = require('fs')
-console.log("Hello world")
-var a=20;
-var b=30;
-var c=40
-fs.writeFileSync("Hello.txt","Hello i am Abdul samad")
-console.warn(a+b+c)
+const http = require('http')
 
+http.createServer((req,res)=>{
+    res.write("Hello i am abdul samad");
+    res.end()
+}).listen(5000);
