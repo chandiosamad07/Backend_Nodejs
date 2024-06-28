478a96b6bfb11e4f601b3c75f1058741d2c86d11
chandiosamad07 <absamadchandio@gmamil.com>
2024-06-28 16:00:52 +0500

    Render HTML and Json data

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 const express = require ('express')
 const app = express()
 
-app.get('',(req,res)=>{
-   res.send('Hello this is  Home page')
-});
+// app.get('',(req,res)=>{
+//    res.send('Hello this is  Home page')
+// });
 
-app.get('/about',(req,res)=>{
-   res.send('This is about page')
-});
-app.get('/help',(req,res)=>{
-   res.send('This is Help page')
-});
+// app.get('/about',(req,res)=>{
+//    res.send('This is about page')
+// });
+// app.get('/help',(req,res)=>{
+//    res.send('This is Help page')
+// });
+
+app.get('',(req,res)=>{
+      res.send('<h1>Hello this is  Home page<h1>')
+   });
+   
+   app.get('/about',(req,res)=>{
+      res.send('<input type=text placeholder="username"><br><button>Click me</Button>')
+   });
+   app.get('/help',(req,res)=>{
+      res.send([
+         {
+            name:"abdul samad",
+            email:"absamadchandio@gmail.com"
+         },
+         {
+            name:"abdul samad",
+            email:"absamadchandio@gmail.com"
+         },
+         {
+            name:"abdul samad",
+            email:"absamadchandio@gmail.com"
+         }, {
+            name:"abdul samad",
+            email:"absamadchandio@gmail.com"
+         }
+      ])
+   });
 
 app.listen(5000)
