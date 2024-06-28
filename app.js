4cd3febc75a34fcfc350f7b5397f6e56903fc72b
chandiosamad07 <absamadchandio@gmamil.com>
2024-06-28 16:28:16 +0500

    Middleware

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 const http = require('http')
 
+// app.get('',(req,res)=>{
+//     res.send('<h1>Hello this is  Home page<h1>')
+//  });
+ 
+//  app.get('/about',(req,res)=>{
+//     res.send('<input type=text placeholder="username"><br><button>Click me</Button>')
+//  });
+//  app.get('/help',(req,res)=>{
+//     res.send([
+//        {
+//           name:"abdul samad",
+//           email:"absamadchandio@gmail.com"
+//        },
+//        {
+//           name:"abdul samad",
+//           email:"absamadchandio@gmail.com"
+//        },
+//        {
+//           name:"abdul samad",
+//           email:"absamadchandio@gmail.com"
+//        }, {
+//           name:"abdul samad",
+//           email:"absamadchandio@gmail.com"
+//        }
+//     ])
+//  });
+
+
 http.createServer((req,res)=>{
    res.write(JSON.stringify({name:'Abdul Samad',email:'absamadchandio@gmail.com'}));
    res.end()
--- index.js
+++ index.js
@@ -1,42 +1,27 @@
 const express = require ('express')
 const app = express()
 
-// app.get('',(req,res)=>{
-//    res.send('Hello this is  Home page')
-// });
-
-// app.get('/about',(req,res)=>{
-//    res.send('This is about page')
-// });
-// app.get('/help',(req,res)=>{
-//    res.send('This is Help page')
-// });
-
-app.get('',(req,res)=>{
-      res.send('<h1>Hello this is  Home page<h1>')
-   });
+const reqFilter=(req,res,next)=>{
+   if(!req.query.age){
+      res.send("please provide age")
+   }
+   else{
+      next()
+   }
    
-   app.get('/about',(req,res)=>{
-      res.send('<input type=text placeholder="username"><br><button>Click me</Button>')
-   });
-   app.get('/help',(req,res)=>{
-      res.send([
-         {
-            name:"abdul samad",
-            email:"absamadchandio@gmail.com"
-         },
-         {
-            name:"abdul samad",
-            email:"absamadchandio@gmail.com"
-         },
-         {
-            name:"abdul samad",
-            email:"absamadchandio@gmail.com"
-         }, {
-            name:"abdul samad",
-            email:"absamadchandio@gmail.com"
-         }
-      ])
-   });
+}
+
+app.use(reqFilter)
+app.get('',(req,res)=>{
+   res.send('Hello this is  Home page')
+});
+
+app.get('/about',(req,res)=>{
+   res.send('This is about page')
+});
+app.get('/help',(req,res)=>{
+   res.send('This is Help page')
+});
+
 
 app.listen(5000)
