684a8ef2fdd127c76e010e54ef4ede14aa0ae618
chandiosamad07 <absamadchandio@gmamil.com>
2024-07-08 09:38:14 +0500

    Get API using nodejs

---
+++ api.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dbconnect = require('./mongodb');
+const app =express()
+
+app.get('/',async (req,res)=>{
+    let data = await dbconnect();
+    data = await data.find().toArray();
+    console.log(data)
+    res.send(data)
+})
+
+app.listen(5000)
