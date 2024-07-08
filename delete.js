f89346b028a94627b150582b4026764c4bcc4eef
chandiosamad07 <absamadchandio@gmamil.com>
2024-07-08 09:21:59 +0500

    DeleteMany data from mongodb using nodejs

--- delete.js
+++ delete.js
@@ -2,8 +2,8 @@ const dbconnect =  require('./mongodb');
 
 const deleteData = async()=>{
     let data = await dbconnect();
-    let result = await data.deleteOne({
-        name:'Note 6'
+    let result = await data.deleteMany({
+        name:'Note 7'
     })
     console.warn(result)
 }
