d097ba163aa51fbeb1ae5240078ba7887da52de4
chandiosamad07 <absamadchandio@gmamil.com>
2024-07-08 10:26:35 +0500

    Update data using nodejs

--- api.js
+++ api.js
@@ -2,11 +2,31 @@ const express = require('express');
 const dbconnect = require('./mongodb');
 const app =express()
 
+app.use(express.json());
+
 app.get('/',async (req,res)=>{
     let data = await dbconnect();
     data = await data.find().toArray();
     console.log(data)
     res.send(data)
+})
+
+app.post('/',async (req,res)=>{
+    let data = await dbconnect();
+    let result = await data.insertOne(req.body)
+    res.send(result)
+
+})
+
+app.put('/:name', async(req,res)=>{
+    let data = await dbconnect();
+    let result = await data.updateOne({
+        name:req.params.name
+    },{
+        $set:req.body
+    }
+)
+res.send(result)
 })
 
 app.listen(5000)
