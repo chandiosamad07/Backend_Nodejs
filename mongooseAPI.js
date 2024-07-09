d1fea9aab36010fcbf80162b17b3b247b1187274
chandiosamad07 <absamadchandio@gmamil.com>
2024-07-09 13:45:16 +0500

    delete data using mongoose

--- mongooseAPI.js
+++ mongooseAPI.js
@@ -1,13 +1,24 @@
-const express = require('express')
-require('./config')
-const Product = require('./Product')
+const express = require("express");
+require("./config");
+const Product = require("./Product");
 const app = express();
-app.use(express.json())
-app.post('/create', async (req,res)=>{
-    let data = new Product (req.body)
-    let  result = await data.save();
-    console.log(req.body)
-    res.send(result)
-})
+app.use(express.json());
 
-app.listen(5000)
+app.post("/create", async (req, res) => {
+  let data = new Product(req.body);
+  let result = await data.save();
+  console.log(req.body);
+  res.send(result);
+});
+
+app.get("/list", async (req, res) => {
+  let result = await Product.find();
+  res.send(result);
+});
+
+app.delete("/delete/:_id", async (req, res) => {
+    let result = await Product.deleteOne({ _id: req.params._id });
+    res.send(result);
+  });
+
+app.listen(5000);
