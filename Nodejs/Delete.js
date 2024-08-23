0a2cd56ec295fa08395fc7147ca8418ad305e786
chandiosamad07 <absamadchandio@gmamil.com>
2024-08-23 16:39:24 +0500

    Node js

---
+++ Nodejs/Delete.js
@@ -0,0 +1,10 @@
+const dbconnect = require('./mongodb')
+
+const  deletData = async () =>{
+    let db = await dbconnect()
+    let result = await db.deleteOne({name:"16 pro"})
+   if(result.acknowledged){
+    console.log("Record deleted")
+   }
+} 
+deletData()
---
+++ Nodejs/index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const data = require('./data')
+const db = require('./mongodb')
+
+
+async function main(){
+    let result = await db()
+    let data = await result.find({}).toArray()
+    console.log(data);
+}
+
+main();
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html'); // Set Content-Type to 'text/html'
+//     res.write('<h1>Hello</h1><p>Welcome to my Node.js server!</p>'); // Send HTML content
+//     res.end()
+// });
+
+
+// const server = http.createServer((req,res)=>{
+//     res.writeHead(200,{'Content-type':'application/json'})
+//     res.write(JSON.stringify(data))
+//     res.end();
+// })
+// const port = 5000;
+// server.listen(port, () => {
+//     console.log(`App is running on port ${port}`);
+// });
