a5134c5cc15565419d60f3504e32b28949bf234e
chandiosamad07 <absamadchandio@gmail.com>
2024-08-23 22:09:20 +0500

    Node js server html retrun

--- Nodejs/Server.js
+++ Nodejs/Server.js
@@ -4,11 +4,12 @@ const http = require('http')
 const server = http.createServer((req,res)=>{
 
     res.statusCode=200
-    res.setHeader('Content-Type','text/plain')
-    res.write("Hello this is server")
+    res.setHeader('Content-Type','text/html')
+    res.write("<h1>Hello this is server</h1>")
     res.end()
 })
 
-server.listen(3000,()=>{
-    console.log("App is runing")
+const port =5000
+server.listen(port,()=>{
+    console.log(`App is runing on port ${port}`)
 })
