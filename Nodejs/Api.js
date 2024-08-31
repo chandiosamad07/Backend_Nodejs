093a8a993be35058a76d120ea767ef2ef1305eff
chandiosamad07 <absamadchandio@gmail.com>
2024-08-31 21:39:11 +0500

    Get Data from mongodb using API

--- Nodejs/Api.js
+++ Nodejs/Api.js
@@ -1,2 +1,14 @@
+const express = require('express')
+const mongodb = require('mongodb')
+const app = express()
 const dbconnect = require('./mongodb')
 
+app.get('/',async (req,res)=>{
+    let data = await dbconnect();
+    data = await data.find().toArray();
+    console.log(data);
+    res.send(data);
+
+})
+
+app.listen(5000)
