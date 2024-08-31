31ae82045ad703dbda10fce0ac684dd1e4882dd8
chandiosamad07 <absamadchandio@gmail.com>
2024-08-31 22:13:55 +0500

    Post API using postmen

--- Nodejs/Api.js
+++ Nodejs/Api.js
@@ -3,12 +3,28 @@ const mongodb = require('mongodb')
 const app = express()
 const dbconnect = require('./mongodb')
 
+app.use(express.json())
+
 app.get('/',async (req,res)=>{
     let data = await dbconnect();
     data = await data.find().toArray();
     console.log(data);
     res.send(data);
 
+})
+
+app.post('/',async(req,res)=>{
+    let data = await dbconnect();
+    let result = data.insertOne(req.body);
+    console.log(result)
+    res.send(result)
+})
+
+app.delete('/:id',async (req,res)=>{
+    let data = await dbconnect();
+    let result = await data.deleteOne({_id: new mongodb.ObjectId (req.params.id)})
+    console.log(result)
+    res.send("done")
 })
 
 app.listen(5000)
